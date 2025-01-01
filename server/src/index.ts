2bc24b043b4a3e21fdc35babbf6f508aa4695392
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2025-01-01 12:29:57 -0600

    fix: wrong directory

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,14 @@ Bun.serve({
     port: 3001,
     async fetch(req) {
         if (req.method === "GET") {
-            const path = "./bundle" + new URL(req.url).pathname;
+            const path = "./server/bundle" + new URL(req.url).pathname;
             const e = await exists(path);
             if (e) {
                 const result = await stat(path);
                 if (result.isFile())
                     return new Response(Bun.file(path));
             } 
-            return new Response(Bun.file("./bundle/index.html"));
+            return new Response(Bun.file("./server/bundle/index.html"));
         } else {
             return new Response("Method not allowed", { status: 405 });
         }
